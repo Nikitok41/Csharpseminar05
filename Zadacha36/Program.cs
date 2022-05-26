734c40cfaa43ca511107a0eb3ccc7506eb16c56d
Nikitok41 <nik777pups@gmail.com>
2022-05-26 16:51:38 +0300

    Добавил решение задачек

--- Zadacha36/Program.cs
+++ Zadacha36/Program.cs
@@ -7,3 +7,38 @@
 
 */
 
+// метод создания массива с рандомными значениями
+
+int[] ArrayCreation(int length){ 
+    int[] arr = new int[length]; 
+    Random rnd = new Random(); 
+    for ( int i = 0 ; i< length ; i ++){ 
+        arr[i] = rnd.Next(1,10); 
+    } 
+    return arr;
+}  
+ 
+// метод поиска не чётных индексов и суммирования значений по этим индексов 
+
+int unEvenPositionInArraySum(int[] array) { 
+    int sum = 0; 
+    for (int i = 0; i < array.Length; i++) { 
+        if (i % 2 != 0) 
+            sum += array[i]; 
+    } 
+return sum; 
+} 
+ 
+//вывод в консоль массива
+
+void PrintArray(int [] arr) { 
+    foreach(int item in arr) 
+        Console.Write($"{item} "); 
+}
+
+
+int [] arr = ArrayCreation(10); 
+int res = unEvenPositionInArraySum(arr);  
+PrintArray(arr); 
+Console.WriteLine(); 
+Console.Write($"Сумма нечетных индексов: {res}");
--- Zadacha38/Program.cs
+++ Zadacha38/Program.cs
@@ -5,3 +5,44 @@
 
 */
 
+
+// метод создания массива с рандомными значениями
+
+double [] ArrayCreation(int length) { 
+    double[] arr = new double[length]; 
+    Random rnd = new Random(); 
+    for ( int i = 0 ; i< length ; i ++) { 
+        arr[i] = rnd.Next(1,10); 
+    } 
+    return arr; 
+    }  
+ 
+// метод для нахождения максимального и минимального элемента массива
+
+(double,double) minAndMaxOfArray(double[] array) {  
+    double min = array[0]; 
+    double max = array[0]; 
+    for (int i = 0; i < array.Length; i++){ 
+        if (array[i] < min) 
+            min = array[i]; 
+        if (array[i] > max) 
+            max = array[i]; 
+    } 
+    return (min,max); 
+}
+
+// вывод в консоль массива
+
+void PrintArray(double [] arr) { 
+    foreach(double item in arr) 
+        Console.Write($"{item} "); 
+} 
+ 
+ 
+double [] arr = ArrayCreation(10); 
+double min = 0; 
+double max = 0; 
+(min,max) = minAndMaxOfArray(arr); 
+PrintArray(arr); 
+Console.WriteLine(); 
+Console.WriteLine($"min = {min}, max = {max}, max - min = {max-min}");
